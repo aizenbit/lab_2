c15236936df22865bf7644f5970f29b86a392738
aizenbit <alextopchiy@gmail.com>
2014-03-02 23:18:32 +0400

    Проблема "Failed to load platform plugin „windows“"  успешно "решена".

--- main.cpp
+++ main.cpp
@@ -5,20 +5,9 @@ int main(int argc, char *argv[])
 {
     QStringList paths = QCoreApplication::libraryPaths();
     paths.append(QCoreApplication::applicationDirPath() + "/plugins");
-    paths.append(QCoreApplication::applicationDirPath() + "/plugins/platforms");
-    paths.append("platforms");
     QCoreApplication::setLibraryPaths(paths);
-    //QCoreApplication::addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins/platforms");
+
     QApplication a(argc, argv);
-    /*QStringList list = QCoreApplication::libraryPaths();
-    QLabel label;
-    QString str = "";
-    for (int i = 0;i<list.size();i++)
-    {
-        str += list.at(i) + "\n";
-    }
-    label.setText(str);
-    label.show();*/
     UI ui;
     ui.show();
     return a.exec();
